c12c3085096c7b4d4fbc273832ea8607827e21a2
buterbrot359 <batowka359@gmail.com>
2025-08-20 15:25:26 +0500

    add: Dockerfile and docker-compose

--- cmd/api-server/main.go
+++ cmd/api-server/main.go
@@ -1,16 +1,33 @@
 package main
 
 import (
+	"os"
+
 	"github.com/gin-gonic/gin"
 
 	"net/http"
 )
 
 func main() {
+	dbUser := os.Getenv("POSTGRES_USER")
+	dbPassword:= os.Getenv("POSTGRES_PASSWORD")
+	dbName := os.Getenv("POSTGRES_NAME")
+
+
+
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
+			"dbUser": dbUser,
+			"dbPassword": dbPassword,
+			"dbName": dbName,
+		})
+	})
+	r.GET("/batyr", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"dbUser": dbUser,
+			"dbPassword": dbPassword,
+			"dbName": dbName,
 		})
 	})
 	r.Run()
